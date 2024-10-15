6ebc383690445924afca5667c64094cd8807ddc2
Jacqueline Wilson <5pf7n3x8@students.codeinstitute.net>
2024-10-15 13:44:29 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // ===============================Wait for the DOM to finish loading before running the game
 // ==================================Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
   let buttons = document.getElementsByTagName("button");
 
   for (let button of buttons) {
@@ -10,21 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("You clicked Submit!");
       } else {
         let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+  runGame("addition");
+
 });
+
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
-}
+
+  if (gameType === "addition")
+  {
+    displayAdditionQuestion(num1, num2);
+  }
+  else 
+  {
+      alert(`unknown game type: ${gametype}`)
+      throw `unknown game type: ${gametype}.Aborting!`;
+  }
+  }
 
 
 function checkAnswer() {
@@ -37,27 +52,39 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementScore() {
+function incrementScore()
+{
 
 
 }
 
-function incrementWrongAnswer() {
+function incrementWrongAnswer()
+{
+
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2)
+{
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
+}
+
+function displaySubtractQuestion() 
+{
+
 
 }
 
-function displaySubtractQuestion() {
+function displayMultiplyQuestion() 
+{
+
 
 }
 
-function displayMultiplyQuestion() {
+function displayDivisionQuestion() 
+{
 
-}
-
-function displayDivisionQuestion() {
 
 }
